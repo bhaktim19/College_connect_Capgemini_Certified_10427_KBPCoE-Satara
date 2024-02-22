eef4e9ab5f297f6f96460ef97d11b589c97b1fbe
Bhakti Ganesh Menkar <147612573+bhaktim19@users.noreply.github.com>
2024-02-22 16:53:40 +0530

    Create CalculatorOperations.java

---
+++ Junit Testing/CalculatorOperations.java
@@ -0,0 +1,37 @@
+package com.edu.calc;
+
+public class CalculatorOperations {
+	
+	public int addition() {
+		int a,b,sum;
+		a=10;
+		b=20;
+		sum=a+b;
+		return sum;
+	}
+	
+	public int subtraction() {
+		int a,b;
+		a=20;
+		b=10;
+		int s=a-b;
+		return s;
+	}
+	
+	public int multiplication() {
+		int a,b;
+		a=2;
+		b=4;
+		int m=a*b;
+		return m;
+	}
+	
+	public int division() {
+		int a,b;
+		a=10;
+		b=5;
+		int d=a/b;
+		return d;
+	}
+
+}
